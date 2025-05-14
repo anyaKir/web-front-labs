5adbec5cc15c3090cb248a012cbebabe5eb719f5
Анна Кирдячкина <kirdyachkina2004@gmail.com>
2025-05-14 21:40:50 +0700

    Книги

---
+++ lab6/books.js
@@ -0,0 +1,82 @@
+const books = [
+    {
+        author: 'Джек Лондон',
+        name: 'Сердца трех',
+        publicationYear: 2023,
+        price: 550,
+        bestseller: true,
+        available: true
+    },
+    {
+        author: 'Агата Кристи',
+        name: 'Третья девушка',
+        publicationYear: 2019,
+        price: 430,
+        bestseller: false,
+        available: false
+    },
+    {
+        author: 'Максим Горький',
+        name: 'Трое',
+        publicationYear: 2020,
+        price: 480,
+        bestseller: false,
+        available: true
+    },
+    {
+        author: 'Лев Толстой',
+        name: 'Анна Каренина',
+        publicationYear: 2018,
+        price: 600,
+        bestseller: true,
+        available: true
+    },
+    {
+        author: 'Фёдор Достоевский',
+        name: 'Преступление и наказание',
+        publicationYear: 2021,
+        price: 620,
+        bestseller: true,
+        available: false
+    },
+    {
+        author: 'Дэн Браун',
+        name: 'Код да Винчи',
+        publicationYear: 2020,
+        price: 500,
+        bestseller: true,
+        available: true
+    },
+    {
+        author: 'Рэй Брэдбери',
+        name: '451° по Фаренгейту',
+        publicationYear: 2017,
+        price: 390,
+        bestseller: false,
+        available: true
+    },
+    {
+        author: 'Джоан Роулинг',
+        name: 'Гарри Поттер и философский камень',
+        publicationYear: 2001,
+        price: 700,
+        bestseller: true,
+        available: true
+    },
+    {
+        author: 'Михаил Булгаков',
+        name: 'Мастер и Маргарита',
+        publicationYear: 2022,
+        price: 650,
+        bestseller: true,
+        available: false
+    },
+    {
+        author: 'Александр Пушкин',
+        name: 'Евгений Онегин',
+        publicationYear: 2015,
+        price: 300,
+        bestseller: false,
+        available: true
+    }
+];
